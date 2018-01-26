c7f53dfeaf687bae61043e7ee72a436cbd1fe222
Kris Baillargeon <krisb1220@gmail.com>
2018-01-26 18:58:19 -0500

    Update parameter.js

--- master-js/parameter.js
+++ master-js/parameter.js
@@ -1,30 +1,30 @@
 var Parameter = {
-  variables:  {},
+  variables:  {}, 
   allowedVariables: [],
-  onLoad: function(callback) {
-      window.onload = callback();
-  },
+  helpers: {
+    redundancyChecker: function(varsArray) {
+      varsArray.forEach(function(item){ 
+        let itemSplit = Parameter.variables.array[item].split("="), prop = itemSplit[0], value = itemSplit[1];
+        Parameter.allowedVariables.indexOf(prop) !== -1 ? Parameter.variables[prop] = value : console.log("variable _" +  prop + "_ is not an allowed variable");
+      });
+    }
+  }, 
   parseURL: function(delimeter) {
       let varArray = window.location.href.split(delimeter).slice(1, this.length);
       Parameter.variables.array = varArray;
       return Parameter.variables.array;
   },
-  updateVariables: function(allowed,delimeter) {
-      Parameter.allowedVariables = allowed;
-      let vars = Object.keys(Parameter.variables.array);
-      Parameter.parseURL(delimeter) /* Parse URL */
-      vars.forEach(function(item){
-        let itemSplit = Parameter.variables.array[item].split("=");
-            prop = itemSplit[0],
-            value = itemSplit[1];
-        Parameter.allowedVariables.indexOf(prop) !== -1 ? Parameter.variables[prop] = value : console.log("variable _" +  prop + "_ is not an allowed variable")/* IMPORTANT */; 
-    });
+  updateVariables: function(allowedArray,delimeter) {
+      Parameter.allowedVariables = allowedArray;
+      Parameter.parseURL(delimeter);
+      Parameter.helpers.redundancyChecker(Object.keys(Parameter.variables.array));
+      
   },
-  updateVariablesOnLoad: function(allowedArray, delimeter) {
-    Parameter.onLoad(function(){
+  updateVariablesOnLoad: function(allowedArray, delimeter, callback) {
+    window.onload = function(){
       Parameter.updateVariables(allowedArray, delimeter);
-      return Parameter.variables;
-    });
+    }
   }
+
 }
-Parameter.updateVariablesOnLoad(["hello"], "?");
+Parameter.updateVariablesOnLoad(["variablesToUpdate"], "?"); 
